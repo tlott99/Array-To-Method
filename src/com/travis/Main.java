fa96f6a38d5f864b7b325672181e772ca6846714
tlott99 <travis.lott@svu.edu>
2020-10-03 12:21:44 -0400

    Initial commit

---
+++ src/com/travis/Main.java
@@ -0,0 +1,15 @@
+package com.travis;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int[] arr = {4,5,6,7,8,9};
+    multiply(arr[3],arr[5]);
+    }
+
+    private static void multiply(int i, int i1) {
+        int mult = i * i1;
+        System.out.println(mult);
+    }
+
+}
